d3735865ececcf1b3411c4c9ce8f0e617da9aff7
EnzoGarbim <enzogarbim2004@gmail.com>
2024-04-02 10:28:48 -0300

    Aula2603 e 0204 Renderizadores com Hooks

--- basic/basictools/src/components/Form/Form.jsx
+++ basic/basictools/src/components/Form/Form.jsx
@@ -1,17 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 var teste = 8
 const Form = () => {
     const handleMyEvent = (e) => {
         console.log(e)
         console.log("Ativou o Evento!")
     }
-    const renderButton = (x) => {
-        if (x) {
-            return <h1>Renderizou Bonitinho!</h1>
-        } else {
-            return <h1>Rederizou Lindidinho!!</h1>
-        }
-    }
+    const [x, setX] = useState(true)
     return (
         <>
             <div>
@@ -29,13 +23,13 @@ const Form = () => {
                     }}>Será que você clicaria aqui?</button>
                 </div>
                 <div>
-                    {renderButton(true)}
-                    {renderButton(false)}
+                    <h1>{x ? 'Renderizou' : 'Deu ruim não foi'}</h1>
+                    <button onClick={() => setX(!setX)}>Valida x</button>
                 </div>
             </div>
             <div>
                 <strong>Valor:</strong>{teste}
-                <button onClick={() =>{ teste = 12; console.log(teste)}}>Aperte e mude</button>
+                <button onClick={() => { teste = 12; console.log(teste) }}>Aperte e mude</button>
                 <strong>Valor:</strong>{teste}
             </div>
         </>
